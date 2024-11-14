50a2cc3d92c9a9d955776b9419e6c9130878cb27
Spideyofficial777 <gaminghatyar777@gmail.com>
2024-11-14 17:09:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID =   # api id
+API_HASH = ""  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = ""  # bot token
 
 
 # REDIS
